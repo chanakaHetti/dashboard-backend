779652ebfb5e4828405e4e509d7f96159ab53ac9
chanakaHetti <cdileep40@gmail.com>
2024-07-07 10:44:39 +0100

    Adding the mysql to the project@ @

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dashboard_backend',
+        'USER': 'root',
+        'PASSWORD': '123456789',
+        'HOST': '127.0.0.1', # Default to localhost
+        'PORT': '3306', # Same as the host port specified in Vagrant config
     }
 }
 
